b74db7833f12e1492b1751ea757360308a06ecfe
Denis Kalinin <dionisi2008@gmail.com>
2024-01-06 12:25:24 +0300

    new file:   C2000-PP/Events.txt 	modified:   C2000-PP/Program.cs 	modified:   C2000-PP/bin/Debug/net8.0/C2000-PP.dll 	modified:   C2000-PP/bin/Debug/net8.0/C2000-PP.exe 	modified:   C2000-PP/bin/Debug/net8.0/C2000-PP.pdb 	modified:   C2000-PP/c2000-pp.CS 	modified:   C2000-PP/obj/C2000-PP.csproj.nuget.dgspec.json 	modified:   C2000-PP/obj/Debug/net8.0/C2000-PP.AssemblyInfo.cs 	modified:   C2000-PP/obj/Debug/net8.0/C2000-PP.AssemblyInfoInputs.cache 	modified:   C2000-PP/obj/Debug/net8.0/C2000-PP.GeneratedMSBuildEditorConfig.editorconfig 	modified:   C2000-PP/obj/Debug/net8.0/C2000-PP.assets.cache 	modified:   C2000-PP/obj/Debug/net8.0/C2000-PP.dll 	modified:   C2000-PP/obj/Debug/net8.0/C2000-PP.genruntimeconfig.cache 	modified:   C2000-PP/obj/Debug/net8.0/C2000-PP.pdb 	modified:   C2000-PP/obj/Debug/net8.0/C2000-PP.sourcelink.json 	modified:   C2000-PP/obj/Debug/net8.0/apphost.exe 	modified:   C2000-PP/obj/Debug/net8.0/ref/C2000-PP.dll 	modified:   C2000-PP/obj/Debug/net8.0/refint/C2000-PP.dll 	modified:   C2000-PP/obj/project.nuget.cache

--- C2000-PP/Program.cs
+++ C2000-PP/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
@@ -6,8 +7,18 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             
+            
+            
+            System.Console.WriteLine(string.Join('_', args[1..]) + " = " + args[0] +',');
+            //Чтение_значений_из_нескольких_регистров_флагов = 0x01,
+            Console.WriteLine("Hello World!");
+            ushort dd = 40128;
+            byte[] w = BitConverter.GetBytes(dd);
+            dd = BitConverter.ToUInt16(w);
+            Console.WriteLine(dd);
+
+            //9CBB
         }
     }
 }
--- C2000-PP/obj/Debug/net8.0/C2000-PP.AssemblyInfo.cs
+++ C2000-PP/obj/Debug/net8.0/C2000-PP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C2000-PP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2a934eab12eb40585114229d10b317a49adabaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("C2000-PP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C2000-PP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
